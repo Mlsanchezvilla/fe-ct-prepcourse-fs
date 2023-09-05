7fce4bff6f657d9727ee37170ea7a26f238123c3
Mlsanchezvilla <sanchezvilla.2298@gmail.com>
2023-09-05 13:52:39 -0500

    Modulo 8 terminado

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -5,12 +5,22 @@ function mayuscula(nombre) {
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
+
+  if (nombre && nombre.length > 0) {
+
+  return nombre.charAt(0).toUpperCase() + nombre.slice(1).toLowerCase();
+
+  } else {
+   return " ";
+  }
 }
 
 function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+
+   cb();
 }
 
 function operacionMatematica(num1, num2, cb) {
@@ -26,6 +36,13 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+
+   var resultado= arrayOfNumbers.reduce((total,numero) => total + numero,  0);
+
+   cb(resultado);
+
+   
+
 }
 
 function forEach(array, cb) {
@@ -33,19 +50,54 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
-}
+
+   for (let i = 0; i < array.length; i++) {
+     cb(array[i]);
+
+   }
+
+
+   }
+
+
 
 function map(array, cb) {
    // Debes iterar sobre el arreglo, y cada elemento pasárselo como arguemento al callback.
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
+
+   var nuevoArreglo=[];
+
+   for (let i = 0; i < array.length; i++) {
+    var resultado= cb(array[i]);
+   nuevoArreglo.push(resultado);
+   {
+      return nuevoArreglo;
+   }
+
+
+   };
 }
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+
+   var  resultado = [];
+
+   for (let i = 0; i < arrayOfStrings.length; i++) {
+
+      var cadena = arrayOfStrings[i];
+
+
+      if (cadena.charAt(0).toLowerCase() === "a") {
+         resultado.push(cadena);
+      }
+   }
+
+   return resultado;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
